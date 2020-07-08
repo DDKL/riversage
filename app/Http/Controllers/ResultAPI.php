1ed354fbf9a6d1d5a4cbc345d6cbcebd0a261c20
Duke Lee <dukelee92@yahoo.com>
2020-07-08 09:57:19 -0700

    update resultapi

--- app/Http/Controllers/ResultAPI.php
+++ app/Http/Controllers/ResultAPI.php
@@ -19,8 +19,20 @@ class ResultAPI extends Controller
         $sid = DB::table('responses')->where([['users_id', $users_id], ['responses_id', $rid]])->value('surveys_id');
         if ($sid == 1)
         {
-            $r = DB::table('responses_wellmatrix')->where('responses_id', $rid)->select();
-            return response()->json(['surveys_id' => $sid, $r], 200);
+            //$r = DB::table('responses_wellmatrix')->where('responses_id', $rid)->get();
+            //return response()->json(['surveys_id' => $sid, $r], 200);
+
+            $r = DB::table('responses_wellmatrix')
+                ->join(
+                    'responses', 
+                    function($join) use ($rid)
+                    {
+                        $join->on('responses.responses_id', '=', 'responses_wellmatrix.responses_id')
+                        ->where('responses_wellmatrix.responses_id', '=', $rid);
+                    })
+                ->select('responses.responses_id','responses_wellmatrix.response','responses.surveys_id','responses_wellmatrix.questions_id','responses.users_id')
+                ->get();
+            return response()->json($r);
         }
     }
 }
